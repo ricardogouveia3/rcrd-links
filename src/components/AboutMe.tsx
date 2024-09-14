fa8585e74177a5c16e71987e512d94d40122c6e9
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-09-14 18:45:07 -0300

    Change AboutMe layout to display photo on small screens

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,31 +3,34 @@ import images from "../assets/images";
 const AboutMe = () => {
   return (
     <div className="p-12 bg-quartz-d40 text-white">
-      <h1 className="uppercase text-sm font-black mb-2" id="aboutMe">
-        About me
-      </h1>
-      <div className="flex">
-        <p>
-          Hello, I'm <strong>Ricardo Gouveia</strong>, a front-end developer and
-          UI designer specializing in creating user-friendly and visually
-          appealing interfaces. I'm currently working at{" "}
-          <a
-            href="https://www.thoughtworks.com/pt-br"
-            target="_blank"
-            rel="noopener"
-            className="font-bold underline decoration-pure-purple-500 underline-offset-2"
-          >
-            Thoughtworks
-          </a>
-          . I'm based in São Paulo, Brazil.
-        </p>
-        <div className="flex-none ml-8 w-24 h-24 rounded-full hidden sm:block box-border">
+      <div className="flex flex-col sm:flex-row sm:items-center">
+        <div className="flex-none mb-4 sm:mb-0 sm:mr-8 w-20 h-20 sm:w-24 sm:h-24 rounded-full box-border">
           <img
             loading="lazy"
-            className="mx-auto rounded-full shadow transition-all hover:scale-110 border-4 border-white p-2 hover:p-0 hover:-rotate-6"
+            className="mx-auto rounded-full shadow transition-all sm:hover:scale-110 border-4 border-white sm:p-2 sm:hover:p-0 sm:hover:-rotate-6"
             src={images.gravatar}
-            alt="ricardogouveia3 Gravatar"
+            alt="Ricardo Gouveia Gravatar"
           />
+        </div>
+        <div>
+          <h1 className="uppercase text-sm font-black mb-2" id="aboutMe">
+            About Me
+          </h1>
+          <p>
+            Hello, I'm <strong>Ricardo Gouveia</strong>, a front-end developer
+            and UI designer specializing in creating user-friendly and visually
+            appealing interfaces. I'm currently working at{" "}
+            <a
+              href="https://www.thoughtworks.com/pt-br"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-bold underline decoration-pure-purple-500 underline-offset-2"
+              aria-label="Visit Thoughtworks"
+            >
+              Thoughtworks
+            </a>
+            . I'm based in São Paulo, Brazil.
+          </p>
         </div>
       </div>
     </div>
