426d2cee2f847d7a5ace297b755998660ba6c3f2
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-10-09 08:08:27 -0300

    use pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { EsLinter, linterPlugin, TypeScriptLinter } from "vite-plugin-linter";
+import gzipPlugin from "rollup-plugin-gzip";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  build: {
-    minify: true,
-  },
-});
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    ViteMinifyPlugin({}),
+    ViteImageOptimizer({}),
+    gzipPlugin(),
+    linterPlugin({
+      include: ["./src/**/*.ts", "./src/**/*.tsx"],
+      linters: [
+        new EsLinter({
+          configEnv: configEnv,
+          serveOptions: { clearCacheOnStart: true },
+        }),
+        new TypeScriptLinter(),
+      ],
+      build: {
+        includeMode: "filesInFolder",
+      }
+    }),
+  ],
+}));
