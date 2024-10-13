e937dca08384690182f6ecdcb301948d84788e49
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-10-13 11:59:45 -0300

    copy to clipboard, theme colors cleanup

--- src/components/HtmlSP.tsx
+++ src/components/HtmlSP.tsx
@@ -5,7 +5,7 @@ const htmlSPlogo = getImage(`htmlSPlogo`);
 
 const HtmlSP = () => {
   return (
-    <section className="p-8 sm:p-12 bg-flame-l20 flex flex-col gap-6">
+    <section className="p-8 sm:p-12 bg-flame flex flex-col gap-6">
       <img
         src={htmlSPlogo}
         className="mx-auto hardShadow sm:w-[120px] w-28"
--- src/components/Npx.tsx
+++ src/components/Npx.tsx
@@ -1,9 +1,36 @@
+import { useState } from 'react';
+
 const Npx = () => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText('npx rcrd');
+      setCopied(true);
+
+      // Hide after 2 seconds
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    } catch (err) {
+      console.error('Failed to copy text: ', err);
+    }
+  };
+
   return (
-    <section className="p-1 text-center bg-quartz-d70 overflow-hidden">
-      <div className="inline-block p-6 w-full text-sm sm:text-xl transition-all md:text-2xl text-white hover:scale-110 cursor-pointer">
+    <section className="text-center bg-quartz-d70 overflow-hidden">
+      <button
+        type="button"
+        className="relative inline-block p-6 w-full text-sm sm:text-xl transition-all md:text-2xl text-white hover:scale-110 cursor-pointer bg-transparent border-none"
+        onClick={handleCopy}
+      >
         <code>npx rcrd</code>
-      </div>
+        {copied && (
+          <div className="absolute left-0 right-0 top-1/4 h-1/2 flex justify-center items-center bg-quartz-l50">
+            <span className="text-xs text-quartz font-bold">Copied to clipboard</span>
+          </div>
+        )}
+      </button>
     </section>
   );
 };
