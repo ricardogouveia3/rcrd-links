98935df79aeaa31bc3651baa86d6e24d466bf15c
ricardogouveia3 <ricardogouveia3_@hotmail.com>
2025-08-15 18:19:30 -0300

    improved types for imgs

--- src/types/Link.ts
+++ src/types/Link.ts
@@ -1,11 +1,13 @@
+import {ImageKey} from "../utils/imageMapping.ts";
+
 export interface Link {
   text: string;
   link: string;
   emoji?: string;
-  imageKey?: string;
+  imageKey?: ImageKey;
   isEnabled?: boolean;
   featured?: boolean;
-  emojiFlag?: string;
+  emojiFlag?: ImageKey;
 }
 
 export default Link;
--- src/utils/imageMapping.ts
+++ src/utils/imageMapping.ts
@@ -1,33 +1,35 @@
 import images from "../assets/images";
 
-function getImage(imageKey: string): string | undefined {
-  const imageMap: Record<string, string> = {
-    topLogo: images.logos.rcrdLight,
-    footerLogo: images.logos.rcrdLight,
-    aboutMe: images.gravatar,
-    linkPortfolio: images.logos.rcrdDark,
-    linkBluesky: images.logos.bluesky,
-    linkThreads: images.logos.threads,
-    linkInstagram: images.logos.instagram,
-    linkGitHub: images.logos.github,
-    linkDevto: images.logos.devto,
-    linkBlog: images.logos.blog,
-    linkDribbble: images.logos.dribbble,
-    linkLinkedIn: images.logos.linkedin,
-    linkUnsplash: images.logos.unsplash,
-    linkLens: images.logos.instagram,
-    linkSpeakerDeck: images.logos.speakerdeck,
-    linkMail: images.icons.mail,
-    htmlSPlogo: images.logos.htmlSP,
-    flagPE: images.flags.pernambuco,
-    flagPA: images.flags.para,
-    flagSP: images.flags.saoPaulo,
-    flagBR: images.flags.brazil,
-    emojiFlagBR: images.emoji.brazil,
-    emojiFlagUK: images.emoji.uk,
-  };
+const imageMap = {
+  topLogo: images.logos.rcrdLight,
+  footerLogo: images.logos.rcrdLight,
+  aboutMe: images.gravatar,
+  linkPortfolio: images.logos.rcrdDark,
+  linkBluesky: images.logos.bluesky,
+  linkThreads: images.logos.threads,
+  linkInstagram: images.logos.instagram,
+  linkGitHub: images.logos.github,
+  linkDevto: images.logos.devto,
+  linkBlog: images.logos.blog,
+  linkDribbble: images.logos.dribbble,
+  linkLinkedIn: images.logos.linkedin,
+  linkUnsplash: images.logos.unsplash,
+  linkLens: images.logos.instagram,
+  linkSpeakerDeck: images.logos.speakerdeck,
+  linkMail: images.icons.mail,
+  htmlSPlogo: images.logos.htmlSP,
+  flagPE: images.flags.pernambuco,
+  flagPA: images.flags.para,
+  flagSP: images.flags.saoPaulo,
+  flagBR: images.flags.brazil,
+  emojiFlagBR: images.emoji.brazil,
+  emojiFlagUK: images.emoji.uk,
+} as const;
 
-  return imageMap[imageKey];
+export type ImageKey = keyof typeof imageMap;
+
+function getImage(imageKey: ImageKey): string {
+  return <string>imageMap[imageKey];
 }
 
 export default getImage;
